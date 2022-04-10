294e638bd51d45cbe3b891a927f2634a16fd83f0
Daniel Agorander <daniel.agorander@gmail.com>
2022-04-10 23:41:36 +0200

    Some experiments with terminal input

--- src/main.rs
+++ src/main.rs
@@ -4,26 +4,37 @@
 
 use std::env;
 use std::process;
+use std::io::Write; // Needed to force Print buffer in spite of no newline
 
 fn main() {
+    // Startup busywork, later check for --help etc
     let args: Vec<String> = env::args().collect();
-
-    // NOTE: 1st arg is the name we were invoked with
-    // TODO: Make this call a --help equivalent output
     if args.len() >= 2 {
         println!("I don't support any arguments yet");
     }
-    println!("This is just a test string before taking input.");
-    let texty_text: String = text_io::read!("{}\n");    
-    clear_screen();
-    println!("You wrote:\n");
-    println!("{}", texty_text);
-    // Need to find a way to validate inputs are numbers below...
-    let number_text_1: i32 = text_io::read!();
-    let number_text_2: i32 = text_io::read!();
-    println!("You wrote {} and {}", number_text_1, number_text_2);
+
+    main_screen();
 
     process::exit(0);
+}
+
+fn main_screen() {
+    // Due to the flushing needed with print without newline we should
+    // probably bundle the prints and the std::io::stdout().flush(); in
+    // a separate function to keep things sane
+
+    clear_screen();
+    println!("\n\nSIMPLE CALCULATOR\n\n");
+    print!("State your purpose: ");
+    std::io::stdout().flush();  // TODO: "unused Result that must be used"?SS
+    let purpose: String = take_input();
+    println!("You want to {}", purpose);
+}
+
+fn take_input() -> String {
+    // Look into the whole "try_read" macro?
+    let input: String = text_io::read!("{}\n");
+    return input;
 }
 
 
