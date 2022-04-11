b09df104c39c823701c3b90415958fc5714ec59f
Daniel Agorander <daniel.agorander@gmail.com>
2022-04-11 22:29:30 +0200

    Working on things...

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,47 @@
 
 use std::env;
 use std::process;
-use std::io::Write; // Needed to force Print buffer in spite of no newline
+use std::io::Write; // Needed to force Print buffer in spite of no newline?
 
 fn main() {
-    // Startup busywork, later check for --help etc
-    let args: Vec<String> = env::args().collect();
-    if args.len() >= 2 {
-        println!("I don't support any arguments yet");
-    }
-
+    welcome();
     main_screen();
 
     process::exit(0);
 }
 
-fn main_screen() {
-    // Due to the flushing needed with print without newline we should
-    // probably bundle the prints and the std::io::stdout().flush(); in
-    // a separate function to keep things sane
-
+fn welcome() {
     clear_screen();
     println!("\n\nSIMPLE CALCULATOR\n\n");
-    print!("State your purpose: ");
-    std::io::stdout().flush();  // TODO: "unused Result that must be used"?SS
-    let purpose: String = take_input();
-    println!("You want to {}", purpose);
+    println!("A totally not shit calculator by Turnip McTurnipface.");
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        println!("I probably don't support arguments, yet. Working on that.");
+        println!("Unless you see them working now, then welcome() needs updating.")
+    }
 }
 
-fn take_input() -> String {
-    // Look into the whole "try_read" macro?
+fn main_screen() {
+    let default_input: &str = "This is where you enter text: ";
+    let purpose: String = same_line_input(default_input.to_string());
+    println!("You entered: {}", purpose);
+
+    let test_thingie: &str = "This string tests
+        multiline input
+        to figure out if that works.
+        
+        It automatically includes newlines. :(";
+    println!("{}", test_thingie);
+
+}
+
+fn same_line_input(text: String) -> String {
+    print!("{}", text);
+    std::io::stdout().flush(); // Flush buffer without newline, but why complaining?
     let input: String = text_io::read!("{}\n");
     return input;
 }
-
 
 fn clear_screen() {
     // Because I will forget this import without something super simple
