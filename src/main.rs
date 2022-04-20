c68bc36218001b4e160c15b4d935b6df06f686e4
Daniel Agorander <daniel.agorander@gmail.com>
2022-04-20 23:13:31 +0200

    MVP status achieved - just missing some cleanups and proper content for the info function

--- src/main.rs
+++ src/main.rs
@@ -17,15 +17,47 @@ use std::io::Write; // Needed to force Print buffer in spite of no newline?
 
 // FEATURE WISHLISH:
 // Be able to store results into Memories for later display (or use?)
+// --- Optionally: make it possible to save Memoried values to a text file
 // Remember previous value and access it through arrow-up
 // Support implied multiplication: 10/2(1+1)=10
-// Support returning values and exiting directly if used with params
-// --- probably with a flag to force integers?
-// Give correct error codes on bad exits
+// Give correct error codes on bad exit
+// Make repeating numbers (like 13/3=4.3333333333333333) a bit more sane
+// --- potentially make precision controllable with a flag and option
+// --- which also implies creating an options screen to set things like
+// ------- decimal precision, force integer
 
 fn main() {
-    // TODO: check arguments and do something else if they exist
-    // Start the main loop
+    // Check arguments and do something else if they exist
+    check_for_argument();
+    // We didn't exit by now, means we are in interactive mode
+    interactive_mode();
+    process::exit(0);
+}
+
+fn check_for_argument() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        match meval::eval_str(args[1].as_str()) {
+            Ok(r) => {
+                println!("{}", r.to_string());
+                process::exit(0);
+            }
+            Err(_e) => {
+                match args[1].as_str() {
+                    "h" | "help" | "--help" => { 
+                        help_screen();
+                        process::exit(0);
+                    },
+                    _ => { eprintln!("ERR: use --help or a math expression");
+                        process::exit(1);
+                    }
+                }
+            }
+        }
+    }
+}
+
+fn interactive_mode() {
     loop {
         // Take input
         let input: String = take_input();
@@ -35,7 +67,6 @@ fn main() {
             "quit" | "q" | "exit" | "e" => { break },
             "c" | "clear" => { clear_screen(); continue },
             "h" | "help" => { help_screen(); continue },
-            "i" | "info" => { info_screen(); continue },
             // We now assume the input was mathematical and pass it for solving
             // with a little check to not crash if input was invalid.
             _ => match meval::eval_str(&input) {
@@ -47,13 +78,9 @@ fn main() {
                 }
             }
         };
-
     }
-
-    process::exit(0);
 }
 
-// Still have to figure out why there's that 'static bs here...
 fn take_input() -> String {
     let standard_user_input: &str = "";
     let input: String = same_line_input(standard_user_input.to_string());
@@ -63,24 +90,14 @@ fn take_input() -> String {
 fn help_screen() {
     println!("\nSIMPLE CALCULATOR");
     println!("A totally not shit calculator by Turnip McTurnipface.");
-
-    let args: Vec<String> = env::args().collect();
-    if args.len() > 1 {
-        println!("I probably don't support arguments, yet. Working on that.");
-        println!("Unless you see them working now, then welcome() needs updating.")
-    }
 }
 
 // TODO: Figure out what the heck the 'static is all about.
 // fn info_screen() -> &'static str { return "continue" }
 // etc etc etc. For now, avoiding that through all the Strings... :P
-fn info_screen() {
-    println!("\nSIMPLE CALCULATOR");
-    println!("the boring legal stuff? Or delete this info screen?")  
-}
 
-fn same_line_input(text: String) -> String {
-    print!("{}", text);
+fn same_line_input(prompt: String) -> String {
+    print!("{}", prompt);
     std::io::stdout().flush().unwrap(); // Why need unwrap to silence warning?
         // Maybe it should use a real error handling instead?
     let input: String = text_io::read!("{}\n");
